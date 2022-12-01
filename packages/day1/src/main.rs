406b22d22a0a89e6b7310d0b20ba382a547b7d84
Wouter de Wild <wouter@codewild.nl>
2022-12-01 16:19:26 +0100

    Make use of iter tools

--- packages/day1/src/main.rs
+++ packages/day1/src/main.rs
@@ -1,25 +1,24 @@
+use itertools::Itertools;
+
 fn main() {
     let path = format!("{}/input", env!("CARGO_MANIFEST_DIR"));
     let summed_calories: Vec<usize> = read::file(&path)
         .split("\n\n")
-        .filter_map(|caloric_bag| {
-            let calories: usize = caloric_bag
+        .map(|caloric_bag| {
+            caloric_bag
                 .lines()
                 .filter_map(|amount| amount.parse::<usize>().ok())
                 .into_iter()
-                .sum();
-
-            Some(calories)
+                .sum()
         })
+        .sorted()
+        .rev()
         .collect();
 
     println!("Day One");
     println!("Part one: {}", summed_calories.iter().max().unwrap());
-    println!("Part two: {}", part_two(&summed_calories));
-}
-
-fn part_two(summed_calories: &[usize]) -> usize {
-    let mut sorted_calories: Vec<usize> = summed_calories.to_vec();
-    sorted_calories.sort();
-    sorted_calories.iter().rev().take(3).sum()
+    println!(
+        "Part two: {}",
+        summed_calories.iter().take(3).sum::<usize>()
+    );
 }
