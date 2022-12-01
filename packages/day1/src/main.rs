4ef6174b2aca4fea681a1d3ef7048ada265bf44f
Wouter de Wild <wouter@codewild.nl>
2022-12-01 16:11:04 +0100

    Added day 1 part two

--- packages/day1/src/main.rs
+++ packages/day1/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
     let path = format!("{}/input", env!("CARGO_MANIFEST_DIR"));
     let summed_calories: Vec<usize> = read::file(&path)
@@ -17,4 +15,11 @@ fn main() {
 
     println!("Day One");
     println!("Part one: {}", summed_calories.iter().max().unwrap());
+    println!("Part two: {}", part_two(&summed_calories));
+}
+
+fn part_two(summed_calories: &[usize]) -> usize {
+    let mut sorted_calories: Vec<usize> = summed_calories.to_vec();
+    sorted_calories.sort();
+    sorted_calories.iter().rev().take(3).sum()
 }
