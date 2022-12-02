18e2465d38b697be14985b1ef10b2ae94eb2e252
Wouter de Wild <wouter@codewild.nl>
2022-12-02 10:23:05 +0100

    Part two

--- packages/day2/src/main.rs
+++ packages/day2/src/main.rs
@@ -1,24 +1,38 @@
-use std::str::FromStr;
-
 fn main() {
     let path = format!("{}/input", env!("CARGO_MANIFEST_DIR"));
 
-    let matches: Vec<usize> = read::file(&path)
-        .lines()
-        .map(|line| {
-            let mut moves = line.split_whitespace();
-            let opponent_move = &moves.next().unwrap().to_string();
-            let player_move = &moves.next().unwrap().to_string();
-
-            determine_player_scores(&(opponent_move, player_move))
-        })
-        .collect();
+    let lines: Vec<String> = read::file(&path).lines().map(|c| c.to_string()).collect();
 
     println!("Day Two");
-    println!("Part one: {}", matches.iter().sum::<usize>());
+    println!(
+        "Part one: {}",
+        lines
+            .iter()
+            .map(|line| {
+                let mut moves = line.split_whitespace();
+                let opponent_move = &moves.next().unwrap().to_string();
+                let player_move = &moves.next().unwrap().to_string();
+
+                determine_player_scores_part_one(&(opponent_move, player_move))
+            })
+            .sum::<usize>()
+    );
+    println!(
+        "Part two: {}",
+        lines
+            .iter()
+            .map(|line| {
+                let mut moves = line.split_whitespace();
+                let opponent_move = &moves.next().unwrap().to_string();
+                let player_move = &moves.next().unwrap().to_string();
+
+                determine_player_scores_part_two(&(opponent_move, player_move))
+            })
+            .sum::<usize>()
+    );
 }
 
-fn determine_player_scores(play: &(&str, &str)) -> usize {
+fn determine_player_scores_part_one(play: &(&str, &str)) -> usize {
     match play {
         ("A", "X") => 4,
         ("A", "Y") => 8,
@@ -29,6 +43,21 @@ fn determine_player_scores(play: &(&str, &str)) -> usize {
         ("C", "X") => 7,
         ("C", "Y") => 2,
         ("C", "Z") => 6,
+        _ => 0,
+    }
+}
+
+fn determine_player_scores_part_two(play: &(&str, &str)) -> usize {
+    match play {
+        ("A", "X") => 3,
+        ("A", "Y") => 4,
+        ("A", "Z") => 8,
+        ("B", "X") => 1,
+        ("B", "Y") => 5,
+        ("B", "Z") => 9,
+        ("C", "X") => 2,
+        ("C", "Y") => 6,
+        ("C", "Z") => 7,
         _ => 0,
     }
 }
