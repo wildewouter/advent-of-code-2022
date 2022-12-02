46b0c65c4cedb70d222f033f46587d24cb8aaf12
Wouter de Wild <wouter@codewild.nl>
2022-12-02 10:30:47 +0100

    Simplify some more

--- packages/day2/src/main.rs
+++ packages/day2/src/main.rs
@@ -8,56 +8,44 @@ fn main() {
         "Part one: {}",
         lines
             .iter()
-            .map(|line| {
-                let mut moves = line.split_whitespace();
-                let opponent_move = &moves.next().unwrap().to_string();
-                let player_move = &moves.next().unwrap().to_string();
-
-                determine_player_scores_part_one(&(opponent_move, player_move))
-            })
+            .map(|play| determine_player_scores_part_one(play))
             .sum::<usize>()
     );
     println!(
         "Part two: {}",
         lines
             .iter()
-            .map(|line| {
-                let mut moves = line.split_whitespace();
-                let opponent_move = &moves.next().unwrap().to_string();
-                let player_move = &moves.next().unwrap().to_string();
-
-                determine_player_scores_part_two(&(opponent_move, player_move))
-            })
+            .map(|play| determine_player_scores_part_two(play))
             .sum::<usize>()
     );
 }
 
-fn determine_player_scores_part_one(play: &(&str, &str)) -> usize {
+fn determine_player_scores_part_one(play: &str) -> usize {
     match play {
-        ("A", "X") => 4,
-        ("A", "Y") => 8,
-        ("A", "Z") => 3,
-        ("B", "X") => 1,
-        ("B", "Y") => 5,
-        ("B", "Z") => 9,
-        ("C", "X") => 7,
-        ("C", "Y") => 2,
-        ("C", "Z") => 6,
+        "A X" => 4,
+        "A Y" => 8,
+        "A Z" => 3,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 7,
+        "C Y" => 2,
+        "C Z" => 6,
         _ => 0,
     }
 }
 
-fn determine_player_scores_part_two(play: &(&str, &str)) -> usize {
+fn determine_player_scores_part_two(play: &str) -> usize {
     match play {
-        ("A", "X") => 3,
-        ("A", "Y") => 4,
-        ("A", "Z") => 8,
-        ("B", "X") => 1,
-        ("B", "Y") => 5,
-        ("B", "Z") => 9,
-        ("C", "X") => 2,
-        ("C", "Y") => 6,
-        ("C", "Z") => 7,
+        "A X" => 3,
+        "A Y" => 4,
+        "A Z" => 8,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 2,
+        "C Y" => 6,
+        "C Z" => 7,
         _ => 0,
     }
 }
