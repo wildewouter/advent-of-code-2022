d6750be1574d384c804dcf33e729a0465bd25df5
Wouter de Wild <wouter@codewild.nl>
2022-12-04 10:52:08 +0100

    Day four part two

--- packages/day4/src/main.rs
+++ packages/day4/src/main.rs
@@ -6,27 +6,38 @@ fn main() {
 
     println!("Day Four:");
     println!("Part One: {}", part_one(&input));
+    println!("Part Two: {}", part_two(&input));
 }
 
 fn part_one(input: &str) -> usize {
-    let duplicate_ranges: Vec<Vec<Vec<usize>>> = input
+    input
         .lines()
-        .map(|pair| -> Vec<Vec<usize>> {
-            pair.split(',')
-                .map(|split_pair| -> Vec<usize> {
-                    let positions: Vec<&str> = split_pair.split('-').collect();
-
-                    (positions[0].parse::<usize>().unwrap()
-                        ..(positions[1].parse::<usize>().unwrap()) + 1)
-                        .collect()
-                })
-                .collect()
-        })
+        .map(transform_to_pair_ranges)
         .filter(|pair| {
             let total_intersect = pair[0].intersect(pair[1].clone());
             total_intersect.len() == pair[0].len() || total_intersect.len() == pair[1].len()
         })
-        .collect();
+        .count()
+}
 
-    duplicate_ranges.len()
+fn part_two(input: &str) -> usize {
+    input
+        .lines()
+        .map(transform_to_pair_ranges)
+        .filter(|pair| {
+            let total_intersect = pair[0].intersect(pair[1].clone());
+            !total_intersect.is_empty()
+        })
+        .count()
+}
+
+fn transform_to_pair_ranges(pair: &str) -> Vec<Vec<usize>> {
+    pair.split(',')
+        .map(|split_pair| -> Vec<usize> {
+            let positions: Vec<&str> = split_pair.split('-').collect();
+
+            (positions[0].parse::<usize>().unwrap()..(positions[1].parse::<usize>().unwrap()) + 1)
+                .collect()
+        })
+        .collect()
 }
