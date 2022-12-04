900c14e0647892ef8e63f69530bf4b2b4cb2c3da
Wouter de Wild <wouter@codewild.nl>
2022-12-04 10:42:11 +0100

    Day 4 Part one

---
+++ packages/day4/src/main.rs
@@ -0,0 +1,26 @@
+use array_tool::vec::Intersect;
+
+fn main() {
+    let path = format!("{}/input", env!("CARGO_MANIFEST_DIR"));
+    let ans: Vec<Vec<Vec<usize>>> = read::file(&path)
+        .lines()
+        .map(|pair| -> Vec<Vec<usize>> {
+            pair.split(',')
+                .map(|split_pair| -> Vec<usize> {
+                    let positions: Vec<&str> = split_pair.split('-').collect();
+
+                    (positions[0].parse::<usize>().unwrap()
+                        ..(positions[1].parse::<usize>().unwrap()) + 1)
+                        .collect()
+                })
+                .collect()
+        })
+        .filter(|pair| {
+            let total_intersect = pair[0].intersect(pair[1].clone());
+            total_intersect.len() == pair[0].len() || total_intersect.len() == pair[1].len()
+        })
+        .collect();
+
+    println!("Day Four:");
+    println!("Part One: {}", ans.len());
+}
